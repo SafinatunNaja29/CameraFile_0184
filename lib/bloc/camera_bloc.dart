048426f1c1bf27a07fa0012bfb37e9de7fdc8258
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-05-15 23:02:44 +0700

    menambahkan logika CameraBloc dan handler event kamera

---
+++ lib/bloc/camera_bloc.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+import 'package:bloc/bloc.dart';
+import 'package:camera/camera.dart';
+import 'package:camerafile/ui/camera_page_bloc.dart';
+import 'package:camerafile/storage_helper_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc//flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import 'camera_event.dart';
+import 'camera_state.dart';
+
+class CameraBloc extends Bloc<CameraEvent, CameraState> {
+  late final List<CameraDescription> _cameras;
+
+  CameraBloc() : super(CameraInitial()) {
+    on<InitializeCamera>(_onInit);
+    on<SwitchCamera>(_onSwitchCamera);
+    on<ToggleFlash>(_onToggleFlash);
+    on<TakePicture>(_onTakePicture);
+    on<TapToFocus>(_onTapFocus);
+    on<PickImageFromGallery>(_onPickGallery);
+    on<OpenCameraAndCapture>(_onOpenCamera);
+    on<DeleteImage>(_onDeleteImage);
+    on<ClearSnackbar>(_onClearSnackBar);
+    on<RequestPermissions>(_onRequestPermissions);
+  }
+
