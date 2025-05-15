974af51e20c2319bfff3c155a0356f0edc6a9fc9
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-05-16 00:57:50 +0700

    Menambahkan inisialisasi kamera dan ikon flash di CameraPage

---
+++ lib/ui/camera_page_bloc.dart
@@ -0,0 +1,33 @@
+import 'package:camera/camera.dart';
+import 'package:camerafile/bloc/camera_event.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'bloc/camera_bloc.dart';
+import 'bloc/camera_state.dart';
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({super.key});
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  @override
+  void initState() {
+    super.initState();
+    final bloc = context.read<CameraBloc>();
+    if (bloc.state is! CameraReady) {
+      bloc.add(InitializeCamera());
+    }
+  }
+
+  IconData _flashIcon(FlashMode mode) {
+    return switch (mode) {
+      FlashMode.auto => Icons.flash_auto,
+      FlashMode.always => Icons.flash_on,
+      _ => Icons.flash_off,
+    };
+  }
+  
+}
